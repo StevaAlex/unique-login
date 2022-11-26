64583892a706eac17574f35d1fde03618e92aaff
Steva <stevaalex@github.com>
2022-11-26 12:30:17 +0000

    Added final code for password

--- starter/script.js
+++ starter/script.js
@@ -111,7 +111,7 @@ function getRandom(arr) {  //make sure to put a name for the array that you will
 // // Function to generate password with user input 
 
 
-var passwordFinal= ''; //put this '' to make it a string so when concatenating it doesn't add numbers
+var passwordFinal= ''; //put this '' to make it a string so when concatenating it doesn't add numbers- This is the final password
 var specialCharSelected = confirm ("Would you like special characters in your password?"); //confirm produces a boolean result
 var numericCharSelected = confirm("Would you like numbers in your password?"); 
 var upperCaseSelected = confirm("Would you like upper case characters in your password?"); 
@@ -119,39 +119,48 @@ var lowerCaseSelected = confirm("Would you like lower case characters in your pa
 var counter = 0;//add this var that allows you to track the type of characters that == true 
 // that way you can calculate how many times the loop in generate2ndPassword runs for
 //so if someone chose 'ok' or true for 3 choices, then counter=3 and if they chose a length of 10, the loop runs 7 times
+
+var preferredCharacters = []; //create this empty array variable to combine/concat the characters variables
+//when the the charsacter variable == true, the preffered variable gets concatenated with that variable 
+//this creates a variable with only the chracters that the user wants in their password
+
 function generatePassword() {  
 
   if (specialCharSelected == true) { 
    passwordFinal += getRandom(specialCharacters);//if ==true generate random array selection and concat to 
-   counter++;
+   counter++; 
+    preferredCharacters= preferredCharacters.concat(specialCharacters);
   } 
   if ( numericCharSelected == true) { 
     passwordFinal += getRandom(numericCharacters);
     counter++;
+    preferredCharacters= preferredCharacters.concat(numericCharacters);
   }
   if ( upperCaseSelected== true) { 
     passwordFinal += getRandom(upperCasedCharacters);
-    counter++;
+    counter++; 
+    preferredCharacters= preferredCharacters.concat(upperCasedCharacters);
   } 
   if ( lowerCaseSelected== true) { 
     passwordFinal += getRandom(lowerCasedCharacters);
-    counter++;
+    counter++; 
+    preferredCharacters= preferredCharacters.concat(lowerCasedCharacters);
 }
 }   
 
 generatePassword(); //call the first function before calling the other function as values 1st function affect the 2nd
 
-var charactersCombined = specialCharacters.concat(numericCharacters, lowerCasedCharacters, upperCasedCharacters); 
-//use concat so that numbers arent added together, it just lists them
 
-function generate2ndPassword() { 
-  for (var i = 0; i < passwordLength - counter ; i++ ) { 
-   passwordFinal+=  getRandom(charactersCombined);
+
+function generateOverallPassword() { 
+  for (var i = 0; i < passwordLength - counter ; i++ ) { //so this subtracts the number of true statements from the obverall password length
+   passwordFinal+=  getRandom(preferredCharacters); //after each loop, a random chracter is selected and stored in a variable
   }
 } 
 
 
-generate2ndPassword(); 
+
+generateOverallPassword(); 
 console.log("overall password: " + passwordFinal); //works! 
 //find a way to randomise passwordFinal, make it hack proof!! 
 //it currently does character, number, upper case and lower case in that order everytime so it needs to be randomised
@@ -162,7 +171,7 @@ var generateBtn = document.querySelector('#generate');
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var password = passwordFinal;
   var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
